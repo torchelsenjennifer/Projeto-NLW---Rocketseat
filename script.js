2f24cb36eccd5d7d1e42ea444c5d073b4763e39a
Jennifer <jennifertorchelsen@gmail.com>
2023-01-20 00:46:09 -0300

    Profissionalizando o projeto

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
 
-const data = {
-  run: ["01-01", "01-02", "01-07", "01-08", "01-06"],
-  takePills: ["01-03"],
-  journal: [""],
+const button = document.querySelector("header button")
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso!")
+    return
+  }
+  alert("Adicionado com sucesso!")
+  nlwSetup.addDay(today)
 }
+
+function save() {
+  localStorage.setItem("jennifer", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("jennifer")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
